4f7cdb95a0f4fa50bdce96d40b59a081e4022a02
kawmauraa <kawamuraalexandre28@gmail.com>
2026-08-27 11:20:26 -0300

    fodasee

--- includes/header.php
+++ includes/header.php
@@ -9,12 +9,24 @@
 </head>
 <body>
     <header class=" bg-slate-900 w-full shadow shadow-gray-950 text-blue-50 mb-2">
-        <nav class="flex justify-between  p-4 text-blue-50">
+        <nav class="flex justify-between items-center p-4 text-blue-50">
             <div class="flex">
-                <div class="SE font-bold bg-amber-400 border-3 border-amber-600 text-blue-50 text-center p-2 mr-1 rounded-lg text-3xl"><p>SE</p></div>
-                <div class=""><p>Sistema</p> <p>Sesc/Senac</p></div>
+                <div class="flex SE font-bold items-center justify-center bg-amber-400 border-3 border-amber-600 text-blue-50 text-center p-2 mr-1 rounded-lg text-3xl"><p class="mb-1 flex text-center justify-center items-center">SE</p></div>  
+                    <div class="ms-1 text-l font-medium mt-1">
+                      <h1 class="text-white">Sistema</h1>
+                      <h1>Sesc/Senac</h1>
+                    </div>
+                </div>
             </div>
-            <div class=" text-xl font-bold flex text-center mt-3 no-underline"><a href="" class="no-underline">depoimentos</a> <a href="" class="ml-10 mr-10  no-underline">cursos</a><a href="" class="mr-10 no-underline">eventos</a><a href="" class="mr-10 no-underline">sobre nós</a><a href="" class=" no-underline">contatos</a></div>
-            <div class="mt-1"><a href=""  class="font-bold bg-amber-400 border-3 border-amber-600 text-blue-50 text-center p-2 rounded-lg text-3xl">Ingressar</a></div>
+            <div class=" text-xl font-semibold flex text-center no-underline">
+                <a href="" class="no-underline">depoimentos</a>
+                <a href="" class="mx-30 no-underline">cursos</a>
+                <a href="" class="no-underline">eventos</a>
+                <a href="" class="mx-30 no-underline">sobre nós</a>
+                <a href="" class="no-underline">contatos</a>
+            </div>
+            <div class="flex text-center items-center">
+                <a href=""  class="flex items-centerfont-semibold bg-amber-400 border-3 border-amber-600 text-blue-50 text-center p-2 rounded-lg text-3xl">Ingressar</a>
+            </div>
         </nav>
     </header>
