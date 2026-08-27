e4daf37aed761d24232b5b555dfb9239c665866a
Asaf-Faza <asafdemelo@gmail.com>
2026-08-27 10:52:36 -0300

    10:52

---
+++ includes/data/banner.php
@@ -0,0 +1,14 @@
+<?php 
+$banner = [
+    [
+        "id" => 1,
+        "nome" => "alex",
+        "imagem" => "./src/banner.png",
+    ],
+    [
+        "id" => 2,
+        "nome" => "praia",
+        "imagem" => "./src/banner.png",
+    ]
+]
+?>
