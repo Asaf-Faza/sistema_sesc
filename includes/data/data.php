3cf594ea2af849d5e8e032e2207b361e76a7fc2f
xandesi <alexsinderski@yahoo.com>
2026-08-31 11:02:39 -0300

    nãopensemoqquiser

--- includes/data/data.php
+++ includes/data/data.php
@@ -16,19 +16,19 @@ $depoimentos = [
         "id" => 1,
         "nome" => "Bernadette Meira",
         "foto" => "src/img/pensemoqquiser.png",
-        "comentario" => "",
+        "comentario" => "digite uma mensagem",
     ],
     [
         "id" => 1,
-        "nome" => "Bernadette Meira",
+        "nome" => "Sandra Lima",
         "foto" => "src/img/pexels-divinetechygirl-1181686.jpg",
         "comentario" => "Adorei muito escravizar meu filho nessa instituição. Ele adquiriu depressão aguda e nunca mais quis conversar com ninguém com medo de falarem sobre PHP para ele de novo (de acordo com ele, isso é pior que vape) 10/10.",
     ],
     [
         "id" => 1,
-        "nome" => "Bernadette Meira",
+        "nome" => "Mair Bocuda",
         "foto" => "src/img/bocuda.png",
-        "comentario" => "Adorei muito escravizar meu filho nessa instituição. Ele adquiriu depressão aguda e nunca mais quis conversar com ninguém com medo de falarem sobre PHP para ele de novo (de acordo com ele, isso é pior que vape) 10/10.",
+        "comentario" => "Não recomendo , talvez se fosse uma instituição mais organizada e séria até gostaria, mas no estado em que se encontra está uma merda",
     ],
 ]
 ?>
